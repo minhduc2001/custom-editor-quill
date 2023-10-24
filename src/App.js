6e202a75b1575ef99788b002665ddcf0804a782a
ducnm <ngoduc2468@gmail.com>
2023-10-24 13:44:56 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import './App.css';
-import Home from './components/Home';
-import Add from './components/Add';
-import Edit from './components/Edit';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
 function App() {
   return (
     <div>
       <Router basename="/">
-        <Switch> 
-          <Route exact path="/" component={Home}/>
-          <Route path="/Add" component={Add}/>
-          <Route path="/Edit/:postID" component={Edit}/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Add" component={Add} />
+          <Route path="/Edit/:postID" component={Edit} />
+          <Route path="/haha"></Route>
         </Switch>
       </Router>
     </div>
